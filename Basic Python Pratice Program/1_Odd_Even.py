ca942b5aa78f3e6f66b75dc4e5dfa24c7ae991b0
CAG07 <craig.gehrig@gmail.com>
2022-10-02 01:38:28 -0500

    Created odd_even.py

---
+++ Basic Python Pratice Program/1_Odd_Even.py
@@ -0,0 +1,10 @@
+#Odd and Even
+#Odd: 1,3,5,...
+#Even: 2,4,6,...
+
+num = int(input("Enter a number: "))
+if num % 2 == 0:
+    print("This is an even number")
+else:
+    print("This is an odd number")
+    
